d39d66afdd8ca1315da81b29d9551e421c6e5415
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-01-18 11:56:38 -0500

    Add Bootstrap and put game in container

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 
 function Square(props) {
@@ -109,15 +110,15 @@ class Game extends Component {
         }
         
         return (
-            <div className="game">
-            <div className="game-board">
-                <Board 
-                    squares={current.squares}
-                    onClick={(i) => this.handleClick(i)}
-                />
-            </div>
-            <div className="game-info">
-                <div>{status}</div>
+            <div className="game container">
+                <div className="game-board">
+                    <Board 
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
+                </div>
+                <div className="game-info">
+                    <div>{status}</div>
                     <ol>{moves}</ol>
                 </div>
             </div>
